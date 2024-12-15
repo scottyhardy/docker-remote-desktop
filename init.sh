348a53d0bcc72ea06c2e9d4d6bc8ebc95ae8ab9f
pponec <pponec@gmail.com>
2024-12-15 21:23:49 +0100

    init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 DOCKER_DIR=$(dirname "$0")
 DOCKER_IMAGE_NAME=remote-desktop
 DOCKER_USER=demo
-function dbuild { docker build -t ${DOCKER_IMAGE_NAME} . ; }
+function dbuild { cd "$DOCKER_DIR" && docker build -t ${DOCKER_IMAGE_NAME} . ; }
 function drun {
   mkdir mkdir -p home/$DOCKER_USER
   docker run -d --shm-size 2g --rm \
@@ -13,7 +13,7 @@ function drun {
          -v $(pwd)/home/$DOCKER_USER:/home/$DOCKER_USER ${DOCKER_IMAGE_NAME};
 }
 function dbash  { docker exec -it ${DOCKER_IMAGE_NAME}_01 "$@" ;}
-function dxrdp  { xfreerdp /v:localhost:3389 /u:$DOCKER_USER ;}
+function dxrdp  { xfreerdp /size:1920x1000 /bpp:32 /v:localhost:3389 /u:$DOCKER_USER ;}
 function dstop  { docker container stop ${DOCKER_IMAGE_NAME}_01 ;}
 function dssh   { ssh $DOCKER_USER@localhost -p 2222 "$@" ;}
 function dall   { dstop; dbuild && drun && sleep 2 && dxrdp ;}
