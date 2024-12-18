5226df32358fd74fa3380881d1a075a5caac4b4a
pponec <pponec@gmail.com>
2024-12-18 22:06:59 +0100

    Documentation and code cleaning

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# docker-remote-desktop
+# ubuntu-docker-desktop-xrdp
 
 [![build](https://github.com/scottyhardy/docker-remote-desktop/actions/workflows/build.yml/badge.svg)](https://github.com/scottyhardy/docker-remote-desktop/actions/workflows/build.yml)
 
-Docker image with RDP server using [xrdp](http://xrdp.org) on Ubuntu with [XFCE](https://xfce.org).
+Docker image with RDP server using [xrdp](http://xrdp.org) on Ubuntu 24.10 LTS with [XFCE](https://xfce.org).
 
 Images are built weekly using the Ubuntu Docker image with the 'latest' tag.
 
--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
 # Common functions for docker
-# Usage: dstop; dstop; dbuild && drun && sleep 2 && dxrdp
+# Usage: . init.sh; dstop; dstop; dbuild; drun; dxrdp
 
 DOCKER_DIR=$(dirname "$0")
 DOCKER_IMAGE_NAME=remote-desktop
 DOCKER_USER=demo
+
+# Build docker image
 function dbuild { cd "$DOCKER_DIR" && docker build -t ${DOCKER_IMAGE_NAME} . ; }
+# Run docker container
 function drun {
   mkdir mkdir -p home/$DOCKER_USER
   docker run -d --rm --shm-size 2g \
@@ -13,24 +16,16 @@ function drun {
          -v "$(pwd)/home/${DOCKER_USER}:/home/${DOCKER_USER}" \
          ${DOCKER_IMAGE_NAME}
 }
+# Run a command or bash.
 function dbash  { docker exec -it ${DOCKER_IMAGE_NAME}_01 "$@" ;}
-function dxrdp  { xfreerdp /size:1920x1000 /bpp:32 /v:localhost:3389 /u:$DOCKER_USER ;}
+# Run Xrdp client.
+function dxrdp  { xfreerdp /size:1200x800 /bpp:32 /v:localhost:3389 /u:$DOCKER_USER ;}
+# Stop the docker container
 function dstop  { docker container stop ${DOCKER_IMAGE_NAME}_01 ;}
+# Run the ssh client.
 function dssh   { ssh $DOCKER_USER@localhost -p 2222 "$@" ;}
+# Run functions: dstop; dbuild; drun; dxrdp
 function dall   { dstop; dbuild && drun && sleep 2 && dxrdp ;}
+# Removes all unused Docker objects including persistent home directory of the docker user `demo`.
 function dpruneall { sudo docker system prune -a && sudo rm -rf "$DOCKER_DIR/home/" ;}
-function dlogs  {
-  cd "$DOCKER_DIR" || exit 1
-  mkdir -p "$DOCKER_DIR/logs/"
 
-  echo "# cat /var/log/xrdp.log" > logs/xrdp.log
-  dbash cat /var/log/xrdp.log   >> logs/xrdp.log
-
-  echo "# cat /var/log/xrdp-sesman.log" > logs/xrdp-sesman.log
-  dbash cat /var/log/xrdp-sesman.log   >> logs/xrdp-sesman.log
-
-  echo "# cat .xorgxrdp.10.log" > logs/xorg.log
-  dbash cat .xorgxrdp.10.log   >> logs/xorg.log
-
-  echo DONE
-}
