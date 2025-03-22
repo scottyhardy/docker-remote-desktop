b96f8e0940ad56edc0a867c0da811bb086525198
scottyhardy <scotthardy42@outlook.com>
2025-03-22 12:50:10 +1100

    feat: replace "ubuntu" account with "user"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-# Delete default ubuntu account if exists
-if id ubuntu > /dev/null 2>&1; then
-    deluser --remove-home ubuntu > /dev/null 2>&1
-fi
-
 # Create the user account
-groupadd --gid 1020 ubuntu
-useradd --shell /bin/bash --uid 1020 --gid 1020 --groups sudo --password "$(openssl passwd ubuntu)" --create-home --home-dir /home/ubuntu ubuntu
+groupadd --gid 1020 user
+useradd --shell /bin/bash --uid 1020 --gid 1020 --groups sudo --password "$(openssl passwd user)" --create-home --home-dir /home/user user
 
 # Start xrdp sesman service
 /usr/sbin/xrdp-sesman
