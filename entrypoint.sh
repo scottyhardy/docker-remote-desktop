d973c8d0f400a21d6476bb9d8a33b30363d2258b
scottyhardy <scotthardy42@outlook.com>
2025-04-04 09:29:27 +1100

    fix: remove old xrdp and sesman pid files on container restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,10 @@ fi
 groupadd --gid 1020 ubuntu
 useradd --shell /bin/bash --uid 1020 --gid 1020 --groups sudo --password "$(openssl passwd ubuntu)" --create-home --home-dir /home/ubuntu ubuntu
 
+# Remove existing sesman/xrdp PID files to prevent rdp sessions hanging on container restart
+[ ! -f /var/run/xrdp/xrdp-sesman.pid ] || rm -f /var/run/xrdp/xrdp-sesman.pid
+[ ! -f /var/run/xrdp/xrdp.pid ] || rm -f /var/run/xrdp/xrdp.pid
+
 # Start xrdp sesman service
 /usr/sbin/xrdp-sesman
 
